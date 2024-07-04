38201d1b2c071545cd60a8b8f074a6b730a387c7
Viraj Sachin <147372921+visa-dev@users.noreply.github.com>
2024-07-04 21:25:08 +0530

    add new front end host url

--- src/main/java/com/fast_food/config/AppConfig.java
+++ src/main/java/com/fast_food/config/AppConfig.java
@@ -50,7 +50,9 @@ public class AppConfig {
 
                 cfg.setAllowedOrigins(List.of(
                         "http://localhost:5173/",
-                        "https://e-commerce-project-zeta-five.vercel.app"
+                        "https://e-commerce-project-zeta-five.vercel.app",
+                        "https://6686c534ddec6c333fe88058--keen-kringle-cefdd7.netlify.app"
+
                 ));
 
                 cfg.setAllowedMethods(Collections.singletonList("*"));
