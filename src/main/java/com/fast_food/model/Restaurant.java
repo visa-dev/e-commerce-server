fb6382e9e26b7a01fee913cae5e67f299a5a6e67
Viraj Sachin <147372921+visa-dev@users.noreply.github.com>
2024-07-08 22:39:07 +0530

    run

--- src/main/java/com/fast_food/model/Restaurant.java
+++ src/main/java/com/fast_food/model/Restaurant.java
@@ -40,10 +40,7 @@ public class Restaurant {
     @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Order> orders = new ArrayList<>();
 
-    @ElementCollection
-    @CollectionTable(name = "restaurant_images", joinColumns = @JoinColumn(name = "restaurant_id"))
-    @Column(name = "image", length = 100)
-    @Cascade(value = org.hibernate.annotations.CascadeType.ALL)
+    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL,orphanRemoval = true)
     private List<String> images = new ArrayList<>();
 
     private LocalDateTime registrationDateTime;
